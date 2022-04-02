8e4730e2f1c0f71fb78d5dfd241ace8526badf53
Maxlego08 <maxlego08@sfr.fr>
2022-04-02 18:08:44 +0200

    :construction: Fix to bootstrap 5

--- resources/views/admin/webhooks/edit.blade.php
+++ resources/views/admin/webhooks/edit.blade.php
@@ -10,8 +10,8 @@
 
                 @include('serveurminecraftvote::admin.webhooks._form')
 
-                <button type="submit" class="btn btn-primary"><i class="fas fa-save"></i> {{ trans('messages.actions.save') }}</button>
-                <a href="{{ route('serveurminecraftvote.admin.rewards.destroy', $reward) }}" class="btn btn-danger" data-confirm="delete"><i class="fas fa-trash"></i> {{ trans('messages.actions.delete') }}</a>
+                <button type="submit" class="btn btn-primary"><i class="bi bi-save"></i> {{ trans('messages.actions.save') }}</button>
+                <a href="{{ route('serveurminecraftvote.admin.rewards.destroy', $reward) }}" class="btn btn-danger" data-confirm="delete"><i class="bi bi-trash"></i> {{ trans('messages.actions.delete') }}</a>
             </form>
         </div>
     </div>
